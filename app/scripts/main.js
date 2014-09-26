2d07558f9e9c1b431ce40b95e473b4797c66fa8c
code-R <vamsi.skrishna@gmail.com>
2014-09-26 14:44:52 +0530

    basic calculator application

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+$(".operand").click(function(){
+  var x = $("#cal-input").val() + $(this).val();
+  $("#cal-input").val(x);
+});
+
+$(".operator").click(function(){
+  var operator = $(this).val();
+  var x = $("#cal-input").val() + operator;
+
+  if(operator == '='){
+    output = eval($("#cal-input").val());
+    $("#cal-input").val(output);
+  }
+  else if(operator == 'c'){
+    $("#cal-input").val('');
+  }
+  else{
+    $("#cal-input").val(x);
+  }
+});
