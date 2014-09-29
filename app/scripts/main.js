5ac46c8f728bd21c4a4d7114d826ea7ec39bbc3c
code-R <vamsi.skrishna@gmail.com>
2014-09-29 11:40:35 +0530

    removing jasmine from bower

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,18 @@
-$(".operand").click(function(){
+$(".operand").click(function() {
   var x = $("#cal-input").val() + $(this).val();
   $("#cal-input").val(x);
 });
 
-$(".operator").click(function(){
+$(".operator").click(function() {
   var operator = $(this).val();
   var x = parseInt($("#cal-input").val()) + operator;
 
-  if(operator == '='){
+  if (operator == '=') {
     output = eval($("#cal-input").val());
     $("#cal-input").val(output);
-  }
-  else if(operator == 'c'){
-    $("#cal-input").val('0');
-  }
-  else{
+  } else if (operator == 'c') {
+    $("#cal-input").val('');
+  } else {
     $("#cal-input").val(x);
   }
 });
