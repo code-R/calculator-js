f2f8b48b9640a4004c7755ea8a2861fc7c74ec00
code-R <vamsi.skrishna@gmail.com>
2014-09-30 11:02:34 +0530

    added btn_class helper method to determine operand and operator class for a button

--- app/scripts/template.js
+++ app/scripts/template.js
@@ -6,68 +6,65 @@ Handlebars.registerHelper('btn_method', function(data) {
   }
 });
 
+Handlebars.registerHelper('btn_class', function(data) {
+  if (data.is_operator) {
+    return 'operator';
+  } else {
+    return 'operand';
+  }
+});
+
 var data1 = {
   data: [{
-    btn_val: '7',
-    btn_class: 'operand'
+    btn_val: '7'
   }, {
-    btn_val: '8',
-    btn_class: 'operand'
+    btn_val: '8'
   }, {
-    btn_val: '9',
-    btn_class: 'operand'
+    btn_val: '9'
   }, {
     btn_val: '+',
-    btn_class: 'operator'
+    is_operator: true
   }]
 }
 
 var data2 = {
   data: [{
-    btn_val: '4',
-    btn_class: 'operand'
+    btn_val: '4'
   }, {
-    btn_val: '5',
-    btn_class: 'operand'
+    btn_val: '5'
   }, {
-    btn_val: '6',
-    btn_class: 'operand'
+    btn_val: '6'
   }, {
     btn_val: '-',
-    btn_class: 'operator'
+    is_operator: true
   }]
 }
 
 var data3 = {
   data: [{
-    btn_val: '1',
-    btn_class: 'operand'
+    btn_val: '1'
   }, {
-    btn_val: '2',
-    btn_class: 'operand'
+    btn_val: '2'
   }, {
-    btn_val: '3',
-    btn_class: 'operand'
+    btn_val: '3'
   }, {
     btn_val: '*',
-    btn_class: 'operator'
+    is_operator: true
   }]
 }
 
 var data4 = {
   data: [{
-    btn_val: '0',
-    btn_class: 'operand'
+    btn_val: '0'
   }, {
-    btn_val: '.',
-    btn_class: 'operand'
+    btn_val: '.'
   }, {
     btn_val: 'C',
-    btn_class: 'operator',
+    is_operator: true,
     btn_function: 'cancel'
   }, {
     btn_val: '=',
-    btn_class: 'operator',
+    is_operator: true,
     btn_function: 'equals'
   }]
 }
